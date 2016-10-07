baad9df33d1d069577e2b0f5aa0bd6efaed24d19
WangTzuHung <kcteamhk2001@gmail.com>
2016-10-07 19:37:52 +0800

    added index action and view in posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :posts do 
+    resources :comments 
+  end
+
   # Example resource route with options:
   #   resources :products do
   #     member do
